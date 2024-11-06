af3fb7be1c9352d47d2feb216bda86f732fab12b
Saif <Saifsep17@gmail.com>
2024-11-06 13:00:34 +0530

    Adjusted Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,18 +12,10 @@ const Navbar = () => {
   const [toggleMenu, setToggleMenu] = React.useState(false);
 
   return (
-    <nav className="w-full flex md:justify-center justify-between items-center p-4">
-      <div className="md:flex-[0.5] flex-initial justify-center items-center">
+    <nav className="w-full flex md:justify-center items-center p-4">
+      <div className="md:flex-[0.5] flex justify-center items-center md:mt-10">
         <img src={logo} alt="logo" className="w-32 cursor-pointer" />
       </div>
-      <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
-        {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
-          <NavBarItem key={item + index} title={item} />
-        ))}
-        <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
-          Login
-        </li>
-      </ul>
       <div className="flex relative">
         {!toggleMenu && (
           <HiMenuAlt4
